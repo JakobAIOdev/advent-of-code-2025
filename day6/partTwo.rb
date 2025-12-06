a9f394a8864c54efd0f6f4c7efee7e893b394737
Jakob Schwendinger <jschwendinger.mmt-b2024@fh-salzburg.ac.at>
2025-12-06 22:05:52 +0100

    add day 6

---
+++ day6/partTwo.rb
@@ -0,0 +1,63 @@
+lines = File.read("input.txt").each_line(chomp: true).to_a
+rows = lines.length
+cols = lines.map(&:length).max
+grid = Array.new(rows) { |r| lines[r].ljust(cols, ' ').chars }
+ops_row = rows - 1
+
+blank = Array.new(cols) do |c|
+  all_blank = true
+  rows.times do |r|
+    if grid[r][c] != ' '
+      all_blank = false
+      break
+    end
+  end
+  all_blank
+end
+
+problems = []
+start = nil
+cols.times do |c|
+  if blank[c]
+    if start
+      problems << [start, c - 1]
+      start = nil
+    end
+  else
+    start ||= c
+  end
+end
+problems << [start, cols - 1] if start
+
+total = 0
+
+problems.each do |start_col, end_col|
+  op = nil
+  (start_col..end_col).each do |c|
+    ch = grid[ops_row][c]
+    if ch == '+' || ch == '*'
+      op = ch
+      break
+    end
+  end
+  next unless op
+
+  nums = []
+  (start_col..end_col).each do |c|
+    digits = ""
+    0.upto(ops_row - 1) do |r|
+      ch = grid[r][c]
+      digits << ch if ch >= '0' && ch <= '9'
+    end
+    nums << digits.to_i unless digits.empty?
+  end
+
+  if op == '+'
+    value = nums.sum
+  else
+    value = nums.inject(1) { |acc, n| acc * n }
+  end
+  total += value
+end
+
+puts total
